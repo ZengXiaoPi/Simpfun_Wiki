75397a74ae870fc854bf7c6762461d552a3b2883
曾小皮-ZengXiaoPi <1768506869@qq.com>
2023-08-07 11:33:59 +0800

    Init

---
+++ docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '简幻欢社区维基',
+  tagline: 'Not Offical',
+  url: 'https://sfe.zxpweb.link',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ZengXiaoPi', // Usually your GitHub org/user name.
+  projectName: 'Simpfun_Wiki', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ZengXiaoPi/Simpfun_Wiki/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ZengXiaoPi/Simpfun_Wiki/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Simpfun 维基',
+        logo: {
+          alt: 'Simpfun',
+          src: 'img/Simpfun.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'main',
+            position: 'left',
+            label: '主页',
+          },
+          {to: '/blog', label: '博客', position: 'left'},
+          {
+            href: 'https://github.com/ZengXiaoPi/Simpfun_Wiki',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '文档',
+            items: [
+              {
+                label: '进入维基',
+                to: '/docs/main',
+              },
+            ],
+          },
+          {
+            title: '友情链接',
+            items: [
+              {
+                label: '简幻欢',
+                href: 'https://simpfun.cn',
+              },
+              {
+                label: '简幻云',
+                href: 'https://simpcloud.cn',
+              },
+              {
+                label: 'BiliBili',
+                href: 'https://space.bilibili.com/1493209225',
+              },
+            ],
+          },
+          {
+            title: '别的东西',
+            items: [
+              {
+                label: '博客',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ZengXiaoPi/Simpfun_Wiki',
+              },
+            ],
+          },
+        ],
+        copyright: `Simpfun Wiki Team 版权所有 由 Docusaurus 构建。`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: '社区维基',
+    description: (
+      <>
+        我们的源码在 Github 开源！
+	    欢迎加入我们的编写！
+      </>
+    ),
+  },
+  {
+    title: '免费试用',
+    description: (
+      <>
+        您可以自由免费查看本维基，
+	    不需要一分钱。
+      </>
+    ),
+  },
+  {
+    title: '最新内容',
+    description: (
+      <>
+        本维基已同步 SFE v4.0 的信息。
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
