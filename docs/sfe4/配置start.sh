d593e338d1c5c716f583cfe387f5da24c6ff3ccc
HanXiMC <109091020+HanXiMC@users.noreply.github.com>
2023-08-16 22:32:04 +0800

    Update 配置start.sh
    修正一个换行错误

--- docs/sfe4/配置start.sh
+++ docs/sfe4/配置start.sh
@@ -17,4 +17,5 @@ title:如何配置start.sh
 5.替换“${openjdk17} -Xms1024M -Xmx${result}M @libraries/net/minecraftforge/forge/此处为版本号/unix_args.txt "$@"”中的版本号为“1.19.2-43.2.0”
 
 6.启动服务器即可
-:::info此时加载时间会比较长
+:::info
+此时加载时间会比较长
